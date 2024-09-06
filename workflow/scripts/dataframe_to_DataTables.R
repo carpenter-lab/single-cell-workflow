efdb043320370f5dc55b74c5360a9a4fe8eadb84
rds156 <rds156@case.edu>
2024-09-06 15:38:24 -0400

    Add Error Handling ----------- Use tryCatch around formatting code to not throw errors when these columns are missing

--- workflow/scripts/dataframe_to_DataTables.R
+++ workflow/scripts/dataframe_to_DataTables.R
@@ -6,15 +6,22 @@ library(data.table)
 
 data <- fread(snakemake@input[["table"]])
 
-data |>
+dt <- data |>
     datatable(
         rownames = FALSE,
         filter = "top",
         options = list(pageLength = 15, lengthMenu = c(5, 10, 15, 20))
-    ) |>
-    formatPercentage(c("pct.1", "pct.2"), 1) |>
-    formatSignif(c("p_val", "p_val_adj")) |>
-    formatRound("avg_log2FC", digits = 3) |>
+    )
+
+dt <- tryCatch(
+    dt |>
+        formatPercentage(c("pct.1", "pct.2"), 1) |>
+        formatSignif(c("p_val", "p_val_adj")) |>
+        formatRound("avg_log2FC", digits = 3),
+    error = function(e) dt
+)
+
+dt |>
     saveWidget(
         snakemake@output[["html"]],
         libdir = fs::path_rel(
